c7f79407a29c499f8cccc3453b0c4a5e707192e7
Byrdann Fox <mobileupstate@gmail.com>
2014-10-25 21:40:57 -0400

    Updates...

--- aacApp/aacApp/ViewController.swift
+++ aacApp/aacApp/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // button...
+    
     var saveShortcutButton:UIButton!
     
     func saveShortcutButtonIsPressed(sender:UIButton) {
         
-        // if...
+        // if shit was saved via core data...
         let alert = UIAlertView()
         alert.title = "Alert"
         alert.message = "Shortcut was saved!"
@@ -23,6 +25,10 @@ class ViewController: UIViewController {
         
     }
     
+    // textview...
+    
+    var textView:UITextView?
+    
     override func viewDidAppear(animated:Bool) {
         super.viewDidAppear(animated)
     }
@@ -30,12 +36,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // button...
         saveShortcutButton = UIButton.buttonWithType(.System) as? UIButton
-        saveShortcutButton.frame = CGRect(x: 110, y: 70, width: 100, height: 44)
+        saveShortcutButton.frame = CGRect(x: 110, y: 240, width: 100, height: 44)
         saveShortcutButton.setTitle("Save", forState: .Normal)
-        saveShortcutButton.setTitle("Saving", forState: .Highlighted)
         saveShortcutButton.addTarget(self, action: "saveShortcutButtonIsPressed:", forControlEvents: .TouchDown)
         view.addSubview(saveShortcutButton)
+        
+        // textview...
+        // !...
+        // ?...
+        textView?.sizeToFit()
+        textView?.layoutIfNeeded()
+        textView = UITextView(frame: CGRect(x: 0, y: 0, width: 320, height: 220))
+        textView?.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0)
+        textView?.font = UIFont.systemFontOfSize(16)
+        textView?.backgroundColor = UIColor.grayColor()
+        view.addSubview(textView!)
         
         // Do any additional setup after loading the view, typically from a nib.
     }
